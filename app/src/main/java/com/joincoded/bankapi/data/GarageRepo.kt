48bb589e8a3093570245315be2db237dab78ea43
mubarak <aloraifan505@hotmail.com>
2024-02-22 15:07:56 +0300

    Done project

--- app/src/main/java/com/joincoded/bankapi/data/GarageRepo.kt
+++ app/src/main/java/com/joincoded/bankapi/data/GarageRepo.kt
@@ -8,68 +8,49 @@ class GarageRepo {
     companion object {
         val garageList = listOf<GarageBranch>(
             GarageBranch(
-                1, "Shwiakh", Type.US,
-                "Near the Co-Op", "97434111",
+                1, "ORP", Type.US,
+                "Shuwaikh Industrial", "1886888",
                 "9:00 -5:00",
-                "https://www.google.com/maps/place/%D8%A8%D9%8A%D8%AA+%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%84+%D8%A7%D9%84%D9%83%D9%88%D9%8A%D8%AA%D9%8A+-+%D9%81%D8%B1%D8%B9+%D8%A7%D9%84%D8%AE%D8%A7%D9%84%D8%AF%D9%8A%D8%A9%E2%80%AD/@29.328966,47.964459,15z/data=!4m2!3m1!1s0x0:0x90397b151c0c8a59?sa=X&ved=2ahUKEwibxeeCi_aDAxXd9gIHHQjRBRkQ_BJ6BAhOEAA",
+"https://maps.app.goo.gl/83tUp3kU9LWfySVS9",
                 R.drawable.orp,
                 reviews = "Good Place"
             ),
             GarageBranch(
-                2, "Pro Rider", Type.GERMAN,
-                " Sports Club", "457353998",
+                2, " Al-Mailam ", Type.GERMAN,
+                "Canada Dry Street", "1884737",
                 "9:00 - 5:00",
-                "https://www.google.com/maps/place/%D8%A8%D9%8A%D8%AA+%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%84+%D8%A7%D9%84%D9%83%D9%88%D9%8A%D8%AA%D9%8A+-+%D9%81%D8%B1%D8%B9+%D9%83%D9%8A%D9%81%D8%A7%D9%86%E2%80%AD/@29.3395215,47.9619357,15z/data=!4m2!3m1!1s0x0:0x36b176ac874f5049?sa=X&ved=2ahUKEwjBoMfCi_aDAxUA8gIHHQssAs0Q_BJ6BAhREAA",
-                R.drawable.ko,
+"https://maps.app.goo.gl/46fSitXiDNo5XYF47",
+                R.drawable.unnamed,
                 reviews = "Good Place"
 
             ),
             GarageBranch(
-                3, "AlYarmouk Branch", Type.US,
-                "AlYarmouk, at the back of the KNPC gas station", "+965 24698716",
+                3, "UNDERGROUND RACING -UGR", Type.US,
+                "Shuwaikh Industrial", "+965 96633300",
                 "9:00 - 5:00",
-                "https://www.google.com/maps/place/%D8%A8%D9%8A%D8%AA+%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%84+%D8%A7%D9%84%D9%83%D9%88%D9%8A%D8%AA%D9%8A%E2%80%AD/@29.3094936,47.9634681,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipPVp2Rf_k0wlEyOqyRv-2xjN-y87aE__j1sKOL6!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPVp2Rf_k0wlEyOqyRv-2xjN-y87aE__j1sKOL6%3Dw114-h86-k-no!7i3648!8i2736!4m15!1m7!3m6!1s0x3fcf9ba83373d7f5:0xee024ddfe2ddc380!2z2KjZitiqINin2YTYqtmF2YjZitmEINin2YTZg9mI2YrYqtmK!8m2!3d29.3094927!4d47.9634976!16s%2Fg%2F1x5f8vfl!3m6!1s0x3fcf9ba83373d7f5:0xee024ddfe2ddc380!8m2!3d29.3094927!4d47.9634976!10e5!16s%2Fg%2F1x5f8vfl?entry=ttu",
-                R.drawable.okj,
+"https://maps.app.goo.gl/PgFzTimZBiTcymPJ9",
+                R.drawable.ugr,
                 reviews = "Good Place"
 
             ),
             GarageBranch(
-                4, "Shawikh industruial", Type.GERMAN,
-                "mina shwaikh", "993354576",
+                4, "N.S.P", Type.GERMAN,
+                "Canada Dry", "24830904",
                 "9:00 - 5:00",
-                "https://www.google.com/maps/place/%D8%A8%D9%8A%D8%AA+%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%84+%D8%A7%D9%84%D9%83%D9%88%D9%8A%D8%AA%D9%8A+-+%D9%81%D8%B1%D8%B9+%D8%A7%D9%84%D8%AE%D8%A7%D9%84%D8%AF%D9%8A%D8%A9%E2%80%AD/@29.328966,47.964459,15z/data=!4m2!3m1!1s0x0:0x90397b151c0c8a59?sa=X&ved=2ahUKEwibxeeCi_aDAxXd9gIHHQjRBRkQ_BJ6BAhOEAA",
-                R.drawable.ooo,
+"https://maps.app.goo.gl/kgBxHB5BjrUt7TNHA",
+                R.drawable.alghannam,
                 reviews = "Good Place"
 
             ),
             GarageBranch(
-                5, "Shawikh industruial", Type.GERMAN,
-                "mina shwaikh", "993354576",
+                5, "AlBabtain Auto Canada Dry Street", Type.GERMAN,
+                "Canada Dry Street", "1822500",
                 "9:00 - 5:00",
-                "https://www.google.com/maps/place/%D8%A8%D9%8A%D8%AA+%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%84+%D8%A7%D9%84%D9%83%D9%88%D9%8A%D8%AA%D9%8A+-+%D9%81%D8%B1%D8%B9+%D8%A7%D9%84%D8%AE%D8%A7%D9%84%D8%AF%D9%8A%D8%A9%E2%80%AD/@29.328966,47.964459,15z/data=!4m2!3m1!1s0x0:0x90397b151c0c8a59?sa=X&ved=2ahUKEwibxeeCi_aDAxXd9gIHHQjRBRkQ_BJ6BAhOEAA",
-                R.drawable.ko,
+"https://maps.app.goo.gl/9mTqjgHX9iJXuMPB6",
+                R.drawable.albabtain,
                 reviews = "Good Place"
 
             ),
-            GarageBranch(
-                6, "Shawikh industruial", Type.GERMAN,
-                "mina shwaikh", "993354576",
-                "9:00 - 5:00",
-                "https://www.google.com/maps/place/%D8%A8%D9%8A%D8%AA+%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%84+%D8%A7%D9%84%D9%83%D9%88%D9%8A%D8%AA%D9%8A+-+%D9%81%D8%B1%D8%B9+%D8%A7%D9%84%D8%AE%D8%A7%D9%84%D8%AF%D9%8A%D8%A9%E2%80%AD/@29.328966,47.964459,15z/data=!4m2!3m1!1s0x0:0x90397b151c0c8a59?sa=X&ved=2ahUKEwibxeeCi_aDAxXd9gIHHQjRBRkQ_BJ6BAhOEAA",
-                R.drawable.ooo,
-                reviews = "Good Place"
-
-            ),
-            GarageBranch(
-                7, "Shawikh industruial", Type.GERMAN,
-                "mina shwaikh", "993354576",
-                "9:00 - 5:00",
-                "https://www.google.com/maps/place/%D8%A8%D9%8A%D8%AA+%D8%A7%D9%84%D8%AA%D9%85%D9%88%D9%8A%D9%84+%D8%A7%D9%84%D9%83%D9%88%D9%8A%D8%AA%D9%8A+-+%D9%81%D8%B1%D8%B9+%D8%A7%D9%84%D8%AE%D8%A7%D9%84%D8%AF%D9%8A%D8%A9%E2%80%AD/@29.328966,47.964459,15z/data=!4m2!3m1!1s0x0:0x90397b151c0c8a59?sa=X&ved=2ahUKEwibxeeCi_aDAxXd9gIHHQjRBRkQ_BJ6BAhOEAA",
-                R.drawable.orp,
-                reviews = "Good Place"
-
-            ),
-
             )
     }
 }
